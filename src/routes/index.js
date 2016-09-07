fdd50a8e2e445548437847b917de4dc431696719
Steve King, Jr <stevekx86@gmail.com>
2016-09-07 11:44:43 -0700

    User creation

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var validate = require('express-jsonschema').validate;
+var userAddSchema = require('../schema/useradd');
+var execFile = require('child_process').execFile;
+var shellescape = require('shell-escape');
+var passwd = require('passwd-linux');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('Pippen v0.1.0');
+});
+
+router.post('/useradd', validate({body: userAddSchema}), (req, res, next) => {
+    var body = request.body;
+    const useradd = execFile('useradd', [body.username], (err, stdout, stderr) => {
+        if (err) {
+            res.status(500);
+            res.send('Could not create user');
+        }
+        else {
+            const mkhomedir_helper = execFile('mkhomedir_helper' [body.username], (err, stdout, stderr) => {
+                if (err) {
+                    res.status(500);
+                    res.send('Could not make home directory for user');
+                }
+                else {
+                    passwd.changePass(body.username, body.password, (err, response) => {
+                        if (err) {
+                            res.status(500);
+                            res.send('Could not set password for user');
+                        }
+                        else {
+                            res.status(201);
+                            res.send('User created');
+                        }
+                    });
+                }
+            });
+        }
+    });
 });
 
 module.exports = router;
