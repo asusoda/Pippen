804a58877fcbc66d7325a6cc887e1071fbc7a6a9
Steve King, Jr <stevekx86@gmail.com>
2016-09-07 11:56:00 -0700

    Add pug to dependencies, rely on pug for views

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.use('/', routes);
 
 app.use(validateMiddleware);
