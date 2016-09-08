4f051565301bd6256f3b08c27b736b888f9e342f
Steve King, Jr <stevekx86@gmail.com>
2016-09-07 17:25:53 -0700

    Schema for password change

---
+++ src/schema/changePassword.js
@@ -0,0 +1,19 @@
+var ChangePasswordSchema = {
+    type: 'object',
+    properties: {
+        username: {
+            type: 'string',
+            required: true
+        },
+        current_password: {
+            type: 'string',
+            required: true
+        },
+        new_password: {
+            type: 'string',
+            required: true
+        }
+    }
+};
+
+module.exports = ChangePasswordSchema;
